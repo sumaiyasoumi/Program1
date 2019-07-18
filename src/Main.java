3652095c3728d4f97a62b40f6b4488fd525f13ea
jubaer <mdjasont@gmail.com>
2019-07-18 10:06:05 +0600

    In this program1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
         System.out.println("Hello World!");
         System.out.println("Hello");
+        int a=2;
+        int b=3;
+        c=(a+b);
+        System.out.println("c");
+
     }
 }
